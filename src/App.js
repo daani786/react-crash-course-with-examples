cd650941416c32170a429f0da7202f65a8578ca1
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-05 22:10:56 +0500

    Exercise Add decrement button On zero value disable decrement button Adjust html layout Add Reset button in counters.jsx component Change tab from 2 space to 4 space length

--- src/App.js
+++ src/App.js
@@ -4,76 +4,79 @@ import "./App.css";
 import Counters from "./components/counters";
 
 class App extends Component {
-  state = {
-    name: "Counters",
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
+    state = {
+        name: "Counters",
+        counters: [
+            { id: 1, value: 4 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 }
+        ]
+    };
 
-  // 'constructor' (1st) hook of the MOUNTing phase
-  constructor(props) {
-    super(props);
-    console.log("App - Constructor", this.props);
-    // this.state = this.props.something.... // can be set state directly to the props here
-  }
+    // 'constructor' (1st) hook of the MOUNTing phase
+    constructor(props) {
+        super(props);
+        //console.log("App - Constructor", this.props);
+        // this.state = this.props.something.... // can be set state directly to the props here
+    }
 
-  // 'render' (2nd) hook of the MOUNTing phase
-  render() {
-    console.log("App - Rendered"); // along with all its children recursively
-    return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
-          onReset={this.handleReset}
-        />
-        <main>
-          <Counters
-            counters={this.state.counters}
-            onDecrement={this.handleDecrement}
-            onIncrement={this.handleIncrement}
-            onDelete={this.handleDelete}
-          />
-        </main>
-      </React.Fragment>
-    );
-  }
+    // 'render' (2nd) hook of the MOUNTing phase
+    render() {
+        //console.log("App - Rendered"); // along with all its children recursively
+        return (
+            <React.Fragment>
+                <NavBar
+                    totalCounters={
+                        this.state.counters.filter(c => c.value > 0).length
+                    }
+                    onReset={this.handleReset}
+                />
+                <main className="container">
+                    <Counters
+                        counters={this.state.counters}
+                        onDecrement={this.handleDecrement}
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}
+                        onReset={this.handleReset}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
 
-  // 'componentDidMount' (3rd) hook of the MOUNTing phase
-  componentDidMount() {
-    // perfect place to make AJAX call to get data from the server
-    console.log("App - Mounted");
-    // this.setState(to the recieved data...)
-  }
+    // 'componentDidMount' (3rd) hook of the MOUNTing phase
+    componentDidMount() {
+        // perfect place to make AJAX call to get data from the server
+        //console.log("App - Mounted");
+        // this.setState(to the recieved data...)
+    }
 
-  /* Children's event handlers */
-  handleDecrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index].value--;
-    this.setState({ counters });
-  };
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index].value++;
-    this.setState({ counters });
-  };
-  handleDelete = counterId => {
-    // return all the counter objects except the one with the given id (the del btn of which is clicked)
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters }); // polluting the state of the counters
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
+    /* Children's event handlers */
+    handleDecrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index].value--;
+        this.setState({ counters });
+    };
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index].value++;
+        this.setState({ counters });
+    };
+    handleDelete = counterId => {
+        // return all the counter objects except the one with the given id (the del btn of which is clicked)
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters }); // polluting the state of the counters
+    };
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,24 +2,37 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  render() {
-    console.log("Counters - Rendered");
-    // 'Object destructuring' applied
-    const { counters, onDecrement, onIncrement, onDelete } = this.props;
-    return (
-      <div>
-        {counters.map(counter => (
-          <Counter
-            key={counter.id}
-            onDecrement={onDecrement}
-            onIncrement={onIncrement}
-            onDelete={onDelete}
-            counter={counter} // this 'counter' object encapsulates all the props / data within it and save us from redundant explicit prop declaration
-          />
-        ))}
-      </div>
-    );
-  }
+    render() {
+        //console.log("Counters - Rendered");
+        // 'Object destructuring' applied
+        const {
+            counters,
+            onDecrement,
+            onIncrement,
+            onDelete,
+            onReset
+        } = this.props;
+        return (
+            <div>
+                <button
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2"
+                >
+                    Reset
+                </button>
+
+                {counters.map(counter => (
+                    <Counter
+                        key={counter.id}
+                        onDecrement={onDecrement}
+                        onIncrement={onIncrement}
+                        onDelete={onDelete}
+                        counter={counter} // this 'counter' object encapsulates all the props / data within it and save us from redundant explicit prop declaration
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Counters;
