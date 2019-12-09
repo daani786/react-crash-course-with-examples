5c2872980e1d3c77d9d3eb4653c660cac06c05fb
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-09 11:58:34 +0500

    04. Composing Components     21- Exercise- Like Component     22- Solution- Like Component Add font awesome Remove components navbar, counters and counter

--- src/App.js
+++ src/App.js
@@ -1,82 +1,15 @@
 import React, { Component } from "react";
-import NavBar from "./components/navbar";
 import "./App.css";
-import Counters from "./components/counters";
+import Movies from "./components/movies";
 
 class App extends Component {
-    state = {
-        name: "Counters",
-        counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-    };
-
-    // 'constructor' (1st) hook of the MOUNTing phase
-    constructor(props) {
-        super(props);
-        //console.log("App - Constructor", this.props);
-        // this.state = this.props.something.... // can be set state directly to the props here
-    }
-
-    // 'render' (2nd) hook of the MOUNTing phase
     render() {
-        //console.log("App - Rendered"); // along with all its children recursively
         return (
-            <React.Fragment>
-                <NavBar
-                    totalCounters={
-                        this.state.counters.filter(c => c.value > 0).length
-                    }
-                    onReset={this.handleReset}
-                />
-                <main className="container">
-                    <Counters
-                        counters={this.state.counters}
-                        onDecrement={this.handleDecrement}
-                        onIncrement={this.handleIncrement}
-                        onDelete={this.handleDelete}
-                        onReset={this.handleReset}
-                    />
-                </main>
-            </React.Fragment>
+            <main className="container">
+                <Movies />
+            </main>
         );
     }
-
-    // 'componentDidMount' (3rd) hook of the MOUNTing phase
-    componentDidMount() {
-        // perfect place to make AJAX call to get data from the server
-        //console.log("App - Mounted");
-        // this.setState(to the recieved data...)
-    }
-
-    /* Children's event handlers */
-    handleDecrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value--;
-        this.setState({ counters });
-    };
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value++;
-        this.setState({ counters });
-    };
-    handleDelete = counterId => {
-        // return all the counter objects except the one with the given id (the del btn of which is clicked)
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters }); // polluting the state of the counters
-    };
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({ counters });
-    };
 }
 
 export default App;
