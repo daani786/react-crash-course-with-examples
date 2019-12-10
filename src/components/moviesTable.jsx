d52dd7d54e7911602f830b84b1790fdb0790f00c
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-10 10:04:29 +0500

    05. Pagination, Filtering, and Sorting     24- Sorting- Extracting Table

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 
 import Like from "./common/like";
-import TableBody from "./common/tableBody";
-import TableHeader from "./common/tableHeader";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
     columns = [
@@ -36,14 +35,12 @@ class MoviesTable extends Component {
         const { movies, onSort, sortColumn } = this.props;
 
         return (
-            <table className="table">
-                <TableHeader
-                    columns={this.columns}
-                    sortColumn={sortColumn}
-                    onSort={onSort}
-                />
-                <TableBody data={movies} columns={this.columns} />
-            </table>
+            <Table
+                columns={this.columns}
+                sortColumn={sortColumn}
+                onSort={onSort}
+                data={movies}
+            />
         );
     }
 }
