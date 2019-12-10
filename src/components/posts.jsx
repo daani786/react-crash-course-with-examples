49ec937ee1c12d584f5d022d7b10e893f2f8f253
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-10 12:45:17 +0500

    06. Routing     10- Query String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+    //http://localhost:3000/posts?sortBy=newest&approved=true
+    const { sortBy, approved } = queryString.parse(location.search);
+    console.log(sortBy, approved);
     return (
         <div>
             <h2>Posts</h2>
