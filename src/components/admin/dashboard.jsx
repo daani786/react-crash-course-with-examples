0707fb0bb7ec0c5fee3ea78df8861d2dba4dbdfe
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-10 13:07:00 +0500

    06. Routing     13- Nested Routing

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,7 +1,18 @@
+import Posts from "./posts";
 import React from "react";
+import { Route } from "react-router-dom";
+import SideBar from "./sidebar";
+import Users from "./users";
 
-const Dashboard = () => {
-    return <h2>Dashboard</h2>;
+const Dashboard = ({ match }) => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <SideBar />
+            <Route path="/admin/users" component={Users} />
+            <Route path="/admin/posts" component={Posts} />
+        </div>
+    );
 };
 
 export default Dashboard;
