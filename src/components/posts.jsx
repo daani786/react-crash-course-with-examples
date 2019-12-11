6a863461b16ea3b6cd9680f91bae0207ea021b32
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-11 13:16:08 +0500

    08. Calling Backend Services     9- Optimistic vs Pessimistic Updates

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -33,10 +33,18 @@ class Posts extends Component {
         this.setState({ posts });
     };
     handleDelete = async post => {
-        await axios.delete(apiEndpoint + "/" + post.id);
+        const originalPosts = this.state.posts;
 
         const posts = this.state.posts.filter(p => p.id !== post.id);
         this.setState({ posts });
+
+        try {
+            await axios.delete(apiEndpoint + "/" + post.id);
+            throw new Error("");
+        } catch (ex) {
+            alert("Something failed while deleting a post!");
+            this.setState({ posts: originalPosts });
+        }
     };
 
     render() {
