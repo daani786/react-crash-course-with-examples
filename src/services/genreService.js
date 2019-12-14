c09607f25b364573efb169a2410f2c8fd2c593e7
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-14 15:13:10 +0500

    08. Calling Backend Services     26- Extracting a Config File

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,5 +1,6 @@
+import { apiUrl } from "../config.json";
 import http from "../services/httpService";
 
 export function getGenres() {
-    return http.get("http://localhost:3900/api/genres");
+    return http.get(apiUrl + "/genres");
 }
