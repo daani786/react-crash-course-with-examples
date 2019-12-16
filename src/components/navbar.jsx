5d427eb3724bafa012a4c01c4caee39fec39d9e4
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-16 16:08:59 +0500

    09. Authentication and Authorization     12- Displaying the Current User on NavBar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 
 import React from "react";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">
@@ -21,24 +21,46 @@ const NavBar = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <NavLink className="nav-item nav-link" to="/movies">
-                        Movies
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/customers">
-                        Customers
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/rentals">
-                        Rentals
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/posts">
-                        Posts
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/login">
-                        Login
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/register">
-                        Register
-                    </NavLink>
+                    <React.Fragment>
+                        <NavLink className="nav-item nav-link" to="/movies">
+                            Movies
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/customers">
+                            Customers
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/rentals">
+                            Rentals
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/posts">
+                            Posts
+                        </NavLink>
+                    </React.Fragment>
+                    {!user && (
+                        <React.Fragment>
+                            <NavLink className="nav-item nav-link" to="/login">
+                                Login
+                            </NavLink>
+                            <NavLink
+                                className="nav-item nav-link"
+                                to="/register"
+                            >
+                                Register
+                            </NavLink>
+                        </React.Fragment>
+                    )}
+                    {user && (
+                        <React.Fragment>
+                            <NavLink
+                                className="nav-item nav-link"
+                                to="/profile"
+                            >
+                                {user.name}
+                            </NavLink>
+                            <NavLink className="nav-item nav-link" to="/logout">
+                                Logout
+                            </NavLink>
+                        </React.Fragment>
+                    )}
                 </div>
             </div>
         </nav>
