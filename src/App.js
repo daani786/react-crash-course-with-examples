7efbbf545cf16978c272d2afbf6ef63e17eccdd2
Adnan Masood <m.adnan.masood@gmail.com>
2019-12-16 16:12:20 +0500

    09. Authentication and Authorization     13- Logging out a User

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 
 import Customers from "./components/customers";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/logout";
 import MovieForm from "./components/movieForm";
 import Movies from "./components/movies";
 import NavBar from "./components/navbar";
@@ -34,6 +35,7 @@ class App extends Component {
                 <main className="container">
                     <Switch>
                         <Route path="/login" component={LoginForm}></Route>
+                        <Route path="/logout" component={Logout}></Route>
                         <Route
                             path="/register"
                             component={RegisterForm}
